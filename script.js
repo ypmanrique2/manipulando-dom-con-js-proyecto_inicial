583e9c713270996d8289191b615186cd8cda311b
YADIN PAULO MANRIQUE MARQUEZ <ypmanrique2@misena.edu.co>
2022-07-19 16:33:43 -0500

    clase02 completada

---
+++ script.js
@@ -0,0 +1,15 @@
+const btn = document.querySelector("[data-form-btn]");
+
+
+const createTask = (evento) => {
+    evento.preventDefault();
+    const input = document.querySelector("[data-form-input]");
+    console.log(input.value);
+}
+
+
+console.log(btn);
+
+
+//Arrow functions o funciones anónimas
+btn.addEventListener("click", createTask);
