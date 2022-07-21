9d22b4f1cd1a3139e6c0e472d042c8730c8a7c27
YADIN PAULO MANRIQUE MARQUEZ <ypmanrique2@misena.edu.co>
2022-07-21 18:17:11 -0500

    manipulacion del DOM con JS

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-( () => {
-    const btn = document.querySelector("[data-form-btn]");
+import checkComplete from "./components/checkComplete.js"
+import deleteIcon from "./components/deleteIcon.js"
+const btn = document.querySelector("[data-form-btn]");
 
     const createTask = (evento) => {
         evento.preventDefault();
@@ -17,30 +18,12 @@
         titleTask.innerText = value;
         taskContent.appendChild(checkComplete());
         taskContent.appendChild(titleTask);
-
-        const content = `
-            <i class="fas fa-trash-alt trashIcon icon"></i>`;
+        //taskContent.appendChild(deleteIcon());
         //task.innerHTML = content;
         task.appendChild(taskContent);
+        task.appendChild(deleteIcon());
         list.appendChild(task);
     };
 
     //Arrow functions o funciones anónimas
     btn.addEventListener("click", createTask);
-
-    const checkComplete = () => {
-        const i = document.createElement("i");
-        i.classList.add("far", "fa-check-square", "icon");
-        i.addEventListener("click", completeTask);
-        return i;
-    };
-    // Inmediately invoked function expression IIFE
-    const completeTask = (event) => {
-        const element = event.target
-        element.classList.toggle("fas");
-        element.classList.toggle("completeIcon");
-        element.classList.toggle("far");
-    };
-
-    
-})();
